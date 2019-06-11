bdcf2a1740a2d127fb42a6668da5d0ba22c00c60
SmartManoj <smartmanoj42857@gmail.com>
2019-06-11 21:36:44 +0530

    'Cool'

--- gt.bat
+++ gt.bat
@@ -1,5 +1,5 @@
 git init
 git add .
 git commit -m 'Cool'
-git remote add origin git@github.com:SmartManoj/cgpa_kec
+rem git remote add origin git@github.com:SmartManoj/cgpa_kec
 git push -u origin master
